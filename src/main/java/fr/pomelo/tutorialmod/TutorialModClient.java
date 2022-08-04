e1ea39a592fe35a5a29d59ea2cfe565f011cf2c5
PomeloJoestar <noe.nizou@gmail.com>
2022-08-04 07:18:19 +0200

    Initial commit

---
+++ src/main/java/fr/pomelo/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package fr.pomelo.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
